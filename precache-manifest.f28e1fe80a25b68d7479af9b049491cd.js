682fd1593ad1e9c11114712ec68f72d5da723476
ross-crawford <rcrawford90@hotmail.co.uk>
2020-05-01 15:38:00 +0100

    Updates

---
+++ precache-manifest.f28e1fe80a25b68d7479af9b049491cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6a5d6526e9929f0b936b7df9ed7a5b",
+    "url": "/RossCrawfordPortfolio/index.html"
+  },
+  {
+    "revision": "acaf9d2b0ae18d9b772b",
+    "url": "/RossCrawfordPortfolio/static/css/main.808adcd4.chunk.css"
+  },
+  {
+    "revision": "960e8fec0cd81a2f83cb",
+    "url": "/RossCrawfordPortfolio/static/js/2.d5e8878a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RossCrawfordPortfolio/static/js/2.d5e8878a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acaf9d2b0ae18d9b772b",
+    "url": "/RossCrawfordPortfolio/static/js/main.553d4d14.chunk.js"
+  },
+  {
+    "revision": "9c12409a993759826a9f",
+    "url": "/RossCrawfordPortfolio/static/js/runtime-main.89ba04de.js"
+  }
+]);
